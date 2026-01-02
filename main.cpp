93edceb5a6fea91118025c869b55b67f57aaaf48
Herin Soni <herinsoni3737@gmail.com>
2026-01-02 10:18:01 +0530

    allocator: add thread-local heap, debug logs, and heap statistics
    - Make allocator state thread-local using thread_local head
    - Add debug logs for alignment, allocation, reuse, free, and merge
    - Add heap_stats() for used/free memory tracking
    - Improve alignment using bitwise align8
    - Add basic adjacent free block coalescing
    - Extend main to demonstrate reuse

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
+// Minimal Version of malloc with debug logs
+//sbrk(0) returns pointer to top of the memory heap 
+//sbrk(n) returns  (void*)-1 in failure case (While requesting for n memory space)
 #include <iostream>
 #include <unistd.h>
+#include <thread>
 
+using namespace std;
 struct Block {
     size_t size;
     bool free;
@@ -9,38 +14,47 @@ struct Block {
 
 #define META_SIZE sizeof(Block)
 
-Block* entry_point = nullptr;
+thread_local Block* head = nullptr;
+void heap_stats() {
+    size_t used = 0, free = 0;
+    Block* cur = head;
+    while (cur) {
+        if (cur->free) free += cur->size;
+        else used += cur->size;
+        cur = cur->next;
+    }
+    cout << "used=" << used << " free=" << free << endl;
+}
 
 size_t align8(size_t x) {
-    if (x % 8 == 0) return x;
-    return x + (8 - (x % 8));
+    size_t y = (x + 7) & ~7;
+    cout << "align " << x << " -> " << y << endl;
+    return y;
 }
 
 Block* request_space(Block* last, size_t size) {
     Block* block = (Block*)sbrk(0);
-    void* request = sbrk(size + META_SIZE);
-
-    if (request == (void*)-1) {
+    if (sbrk(size + META_SIZE) == (void*)-1)
         return nullptr;
-    }
 
     block->size = size;
     block->free = false;
     block->next = nullptr;
 
-    if (last) {
-        last->next = block;
-    }
+    if (last) last->next = block;
 
+    cout << "new block @" << block << " size " << size << endl;
     return block;
 }
 
 Block* find_free_block(size_t size) {
-    Block* curr = entry_point;
-    while (curr) {
-        if (curr->free && curr->size >= size)
-            return curr;
-        curr = curr->next;
+    Block* cur = head;
+    while (cur) {
+        if (cur->free && cur->size >= size) {
+            cout << "reuse block @" << cur << endl;
+            return cur;
+        }
+        cur = cur->next;
     }
     return nullptr;
 }
@@ -51,22 +65,21 @@ void* my_malloc(size_t size) {
     size = align8(size);
     Block* block;
 
-    if (!entry_point) {
+    if (!head) {
         block = request_space(nullptr, size);
-        if (!block) return nullptr;
-        entry_point = block;
+        head = block;
     } else {
         block = find_free_block(size);
         if (!block) {
-            Block* last = entry_point;
+            Block* last = head;
             while (last->next) last = last->next;
             block = request_space(last, size);
-            if (!block) return nullptr;
         } else {
             block->free = false;
         }
     }
 
+    cout << "malloc -> " << (block + 1) << endl;
     return (void*)(block + 1);
 }
 
@@ -76,20 +89,21 @@ void my_free(void* ptr) {
     Block* block = (Block*)ptr - 1;
     block->free = true;
 
+    cout << "free @" << ptr << endl;
+
     if (block->next && block->next->free) {
         block->size += META_SIZE + block->next->size;
         block->next = block->next->next;
+        cout << "merge @" << block << endl;
     }
 }
 
 int main() {
-    int* x = (int*)my_malloc(sizeof(int));
-    *x = 42;
-
-    my_free(x);
-
-    int* y = (int*)my_malloc(sizeof(int));
-    *y = 100;
-
-    std::cout << *y << std::endl;
+    char* a = (char*)my_malloc(sizeof(char));
+    *a = 'A';
+    cout << "user " << *a << endl;
+    my_free(a);
+    int* b = (int*)my_malloc(sizeof(int));
+    *b = 42;
+    cout << "user " << *b << endl;
 }
